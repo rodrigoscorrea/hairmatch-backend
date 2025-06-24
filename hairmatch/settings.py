48ae953c5e799a1dc7ff61c54f70bb84af67d6a7
rodrigoscorrea <rodrigosc616@gmail.com>
2025-06-23 21:25:41 -0400

    fixed wrong format

--- hairmatch/settings.py
+++ hairmatch/settings.py
@@ -149,8 +149,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
-CORS_ALLOWED_ORIGINS = f'{BACKEND_ALLOWED_CORS }' + os.environ.get('BACKEND_ALLOWED_CORS').split(" ")
-CSRF_TRUSTED_ORIGINS = f'{BACKEND_ALLOWED_CORS }' + os.environ.get('BACKEND_ALLOWED_CORS').split(" ")
+CORS_ALLOWED_ORIGINS = (os.environ.get('BACKEND_ALLOWED_CORS').split(" ")).append(f'{BACKEND_ALLOWED_CORS }')
+CSRF_TRUSTED_ORIGINS = (os.environ.get('BACKEND_ALLOWED_CORS').split(" ")).append(f'{BACKEND_ALLOWED_CORS }')
 CORS_ALLOW_CREDENTIALS=True
 AUTH_USER_MODEL = 'users.User'
 

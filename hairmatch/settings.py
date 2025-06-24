9860bbf5af3cc1be092835c700141a63e3e58d65
rodrigoscorrea <rodrigosc616@gmail.com>
2025-06-23 21:30:04 -0400

    .

--- hairmatch/settings.py
+++ hairmatch/settings.py
@@ -149,8 +149,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
-CORS_ALLOWED_ORIGINS = (os.environ.get('BACKEND_ALLOWED_CORS').split(" ")).append(f'{BACKEND_ALLOWED_CORS }')
-CSRF_TRUSTED_ORIGINS = (os.environ.get('BACKEND_ALLOWED_CORS').split(" ")).append(f'{BACKEND_ALLOWED_CORS }')
+CORS_ALLOWED_ORIGINS = list(f'{BACKEND_ALLOWED_CORS }').append(os.environ.get('BACKEND_ALLOWED_CORS').split(" "))
+CSRF_TRUSTED_ORIGINS = list(f'{BACKEND_ALLOWED_CORS }').append(os.environ.get('BACKEND_ALLOWED_CORS').split(" "))
 CORS_ALLOW_CREDENTIALS=True
 AUTH_USER_MODEL = 'users.User'
 

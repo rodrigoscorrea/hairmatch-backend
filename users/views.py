17b185ea44e92646f28f1eeb61b3a61ed575d2bf
Rodrigo <rodrigo.correa@icomp.ufam.edu.br>
2025-06-25 20:30:57 -0400

    removed last updates

--- users/views.py
+++ users/views.py
@@ -376,7 +376,7 @@ class CustomerHomeView(APIView):
                 hairdressers_users = User.objects.filter(
                     role='hairdresser',
                     preferences=preference
-                ).distinct()[:25]
+                ).distinct()[:10]
                 
                 hairdressers_per_preference = Hairdresser.objects.filter(user__in=hairdressers_users)
                 hairdressers_data = HairdresserSerializer(hairdressers_per_preference, many=True).data
